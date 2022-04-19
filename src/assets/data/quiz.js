0fd70b7ee594ae67a31d210e606490165825b7d2
Prince0047 <sonubhadana838@gmail.com>
2022-04-19 11:32:39 +0530

    added question file

---
+++ src/assets/data/quiz.js
@@ -0,0 +1,49 @@
+export default {
+  questions: [
+    {
+      questionText: 'Where is the Headqarters of the ISRO?',
+      answerOptions: [
+        { answerText: 'Pune', isCorrect: false },
+        { answerText: 'Bangalore', isCorrect: true },
+        { answerText: 'Lucknow', isCorrect: false },
+        { answerText: 'Mumbai', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'When was ISRO formed',
+      answerOptions: [
+        { answerText: '15 August 1947', isCorrect: false },
+        { answerText: '15 August 1952', isCorrect: false },
+        { answerText: '15 August 1969', isCorrect: true },
+        { answerText: '15 August 1967', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who is the Father of the Indian Space Program?',
+      answerOptions: [
+        { answerText: 'C.V. Raman', isCorrect: false },
+        { answerText: 'Vikram Sarabhai', isCorrect: true },
+        { answerText: 'M.G.K. Menon', isCorrect: false },
+        { answerText: 'A.P.J. Abdul Kalam', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Where is the Indian Institute of Remote Sensing (IIRS)?',
+      answerOptions: [
+        { answerText: 'Hyderabad', isCorrect: false },
+        { answerText: 'Dehradun', isCorrect: true },
+        { answerText: 'Ahmedabad', isCorrect: false },
+        { answerText: 'Sriharikota', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'What is the Full Form of PSLV?',
+      answerOptions: [
+        { answerText: 'Public Satellite Launch Vehicle', isCorrect: false },
+        { answerText: 'Polar Satellite Launch Vehicle', isCorrect: true },
+        { answerText: 'Polar Service Launch Vehicle', isCorrect: false },
+        { answerText: 'Polar Service Launch Vehicle', isCorrect: false },
+      ],
+    },
+  ],
+};
